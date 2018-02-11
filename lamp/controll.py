14d4691c7eb2d902d36f6683404bc2d4336c11b3
beckstev <beckstev@web.de>
2018-02-11 21:58:07 +0000

    switch workes

--- lamp/controll.py
+++ lamp/controll.py
@@ -1,11 +1,16 @@
 import time
 import phue
 import RPi.GPIO as GPIO
+import logging
+logging.basicConfig()
+
+
 
 SwitchPin = 7
 
 RoAPin = 11    # pin11
 RoBPin = 12    # pin12
+RotatePin = 13
 
 
 
@@ -33,50 +38,57 @@ def on_off(ev = None):
       status = b.get_light(1, 'on')
       b.set_light(1,'on', not status)
 
-def bright( roate_level):
+def bright( rotate_level):
+      print('bright', rotate_level)
       current_bright = b.get_light(1, 'bri')
       if 1<= current_bright + rotate_level <= 254:
-            b.set_light(1,'bri', bright_value + rotate_leve)
+            b.set_light(1,'bri', current_bright + rotate_level)
       elif current_bright + rotate_level > 254:
             b.set_light(1,'bri', 254)
 
       elif current_bright + rotate_level < 1:
+
             b.set_light(1,'bri', 1)
       else:
             None
 
-def rotate():
-      Last_RoB_Status = GPIO.input(RoBPin)
-      while(not GPIO.input(RoAPin)):
-		Current_RoB_Status = GPIO.input(RoBPin)
-		flag = 1
-	if flag == 1:
-		flag = 0
-		if (Last_RoB_Status == 0) and (Current_RoB_Status == 1):
-			globalCounter = globalCounter + 1
-			print 'globalCounter = %d' % globalCounter
-		if (Last_RoB_Status == 1) and (Current_RoB_Status == 0):
-			globalCounter = globalCounter - 1
-			
-      
+def rotate(status_B):
+    counter =0
+    last_status_b = GPIO.input(RoBPin)
+    Flag =1
+    current_b_status = GPIO.input(RoBPin)
+    while(not GPIO.input(RoAPin)):
+          current_b_status = GPIO.input(RoBPin)
+          Flag = 1        
+    if Flag == 1:
+          Flag =0 
+          if (last_status_b == 0) and ( current_b_status == 1):
+                  bright(5)
+          if (last_status_b == 1) and (current_b_status == 0):
+                bright(-5)
+ 
+    return last_status_b
+
       
 def work():
-      GPIO.add_event_detect(SwitchPin, GPIO.FALLING, callback=on_off, bouncetime=200)
-      
+      GPIO.add_event_detect(SwitchPin, GPIO.FALLING, callback=on_off, bouncetime=200)       
+      status_B = GPIO.input(RoBPin)
       while True:
-            time.sleep(1)
+            print(status_B)
+            new_status_b=rotate(status_B)
+            status_B = new_status_b
       
 
       
       
 
 b = connect( get_IP() )
-
 Last_RoB_Status = 0
+b.set_light(1, 'on', True)
+try:
+    setup()
+    work()
+except KeyboardInterrupt: 
 
-bright(1)
-setup()
-work()
-      
-
+    GPIO.cleanup()
       
